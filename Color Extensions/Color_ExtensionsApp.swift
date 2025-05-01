fa6236cf017a7382d793cc10313b9343eddff8fa
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-01 10:30:00 +0800

    - initialized project

---
+++ Color Extensions/Color_ExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_ExtensionsApp.swift
+//  Color Extensions
+//
+//  Created by Ronjie Diafante Man-on on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Color_ExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
